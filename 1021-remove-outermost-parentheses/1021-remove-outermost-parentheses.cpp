f1151f0189d5af0637a0ebf393ac1fd18a52b4ca
Omkar Yandralwar <ody17044@gmail.com>
2026-07-06 10:43:30 +0530

    Time: 0 ms (100%), Space: 9 MB (54.61%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -2,27 +2,16 @@ class Solution {
 public:
     string removeOuterParentheses(string s) {
         if(s.size()<=1) return "";
-        string ans;
-        vector<int> idx;
-        stack<char> temp;
-        for(int i=0;i<s.size();i++){
-            temp.push(s[i]);
-            if(temp.top()==')'){
-                temp.pop();
-                temp.pop();
+        string ans = "";
+        int balance = 0;
+        for(char c : s){
+            if(c == '('){
+                if(balance>0) ans += '(';
+                balance++; 
+            }else{
+                balance--;
+                if(balance>0) ans+=')';
             }
-            if(temp.empty()) idx.push_back(i);     
-        }
-        int j=0;
-        for(int i=1;i<s.size();i++){
-            if (j < idx.size() && idx[j] == i) {
-                continue;
-            }
-            if(j < idx.size() && idx[j]==i-1){
-                j++;
-                continue;
-            }
-            ans.push_back(s[i]);
         }
         return ans;
     }
