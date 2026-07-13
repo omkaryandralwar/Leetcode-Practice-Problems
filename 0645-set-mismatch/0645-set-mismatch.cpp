ca4f06bebbc26115c2a0f5ac9a21b109b440db60
Omkar Yandralwar <ody17044@gmail.com>
2026-07-13 19:58:18 +0530

    Time: 0 ms (100%), Space: 26.3 MB (38.24%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums){
-        sort(nums.begin(),nums.end());
-        int miss;
-        int extra;
-        for(int i=1;i<=nums.size();i++){
-            if(i!=nums.size() && nums[i-1]==nums[i]) extra = nums[i];
-            for(int j =0;j<nums.size();j++){
-                if(nums[j]==i) break;
-                if(j==nums.size()-1 && nums[j]!=i) miss=i;
-            }
+        if(nums.size()<2) return {};
+        vector<int> freq(nums.size()+1, 0);
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++){
+            if(freq[nums[i]]==1) ans.push_back(nums[i]);
+            freq[nums[i]]=1;
         }
-        return {extra, miss};
+        for(int i=1;i<=nums.size();i++){
+            if(freq[i]==0) ans.push_back(i);
+        }
+        return ans;
     }
 };
