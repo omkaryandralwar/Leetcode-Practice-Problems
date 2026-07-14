0710f1d697e8dd28714f007fc10724264633f65f
Omkar Yandralwar <ody17044@gmail.com>
2026-07-14 20:18:35 +0530

    Time: 0 ms (100%), Space: 10.8 MB (23.43%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,20 +1,34 @@
 class Solution {
 public:
     vector<int> temp;
-    int helper(vector<int>& nums, int i){
-        if(i < 0) return 0;
-        if(i == 0) return nums[0];
-        if(temp[i]!=-1){
-            return temp[i];
-        }
-        int robThis = nums[i] + helper(nums, i-2);
-        int skipThis = helper(nums, i-1);
-        temp[i]=max(robThis, skipThis);
-        return max(robThis, skipThis);
-    }
+    // int helper(vector<int>& nums, int i){
+    //     if(i < 0) return 0;
+    //     if(i == 0) return nums[0];
+    //     // if(temp[i]!=-1){
+    //     //     return temp[i];
+    //     // }
+    //     // int robThis = nums[i] + helper(nums, i-2);
+    //     // int skipThis = helper(nums, i-1);
+    //     // temp[i]=max(robThis, skipThis);
+    //     // return max(robThis, skipThis);
+    //     for(int i=2;i<temp.size();i++){
+    //         int robThis = nums[i] + temp[i-2];
+    //         int skipThis = temp[i-1];
+    //         temp[i]=max()
+    //     }
+    // }
 
     int rob(vector<int>& nums) {
-        temp.resize(nums.size()+1, -1);
-        return helper(nums, nums.size()-1);
+        if(nums.size()==0) return 0;
+        if(nums.size()==1) return nums[0];
+        temp.resize(nums.size(), -1);
+        temp[0]=nums[0];
+        temp[1]=max(nums[0], nums[1]);
+        for(int i=2;i<temp.size();i++){
+            int robThis = nums[i] + temp[i-2];
+            int skipThis = temp[i-1];
+            temp[i]=max(robThis, skipThis);
+        }
+        return temp[temp.size()-1];
     }
 };
