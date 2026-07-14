7c7ff541dbe0d267384f13f3ed50bc420629cf56
Omkar Yandralwar <ody17044@gmail.com>
2026-07-14 18:21:09 +0530

    Time: 0 ms (100%), Space: 8.5 MB (37.52%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,20 +2,25 @@ class Solution {
 public:
 vector<int> temp;
     int climbStairs(int n) {
-        if(temp.empty()) temp.resize(n+1, -1);
         if(n == 0 || n == 1) return 1;
-        if(n < 0) return 0;
-        int ans = 0;
-        if(temp[n]>=0){
-            return temp[n];
+        if(temp.empty()) temp.resize(n+1, -1);
+        // if(n == 0 || n == 1) return 1;
+        // if(n < 0) return 0;
+        // int ans = 0;
+        // if(temp[n]>=0){
+        //     return temp[n];
+        // }
+        // int a=climbStairs(n-1);
+        // int b=climbStairs(n-2);
+        // temp[n-1]=a;
+        // temp[n-2]=b;
+        // ans+=a;
+        // ans+=b;
+        // temp[n]=ans;
+        temp[1] = temp[0] = 1;
+        for(int i=2;i<n+1;i++){
+            temp[i]=temp[i-1]+temp[i-2];
         }
-        int a=climbStairs(n-1);
-        int b=climbStairs(n-2);
-        temp[n-1]=a;
-        temp[n-2]=b;
-        ans+=a;
-        ans+=b;
-        temp[n]=ans;
-        return ans;
+        return temp[n];
     }
 };
