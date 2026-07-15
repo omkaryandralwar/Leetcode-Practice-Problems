3a6ca54fdff6d2148a0b59ba2920bdd7217f5d9e
Omkar Yandralwar <ody17044@gmail.com>
2026-07-15 09:54:10 +0530

    Time: 0 ms (100%), Space: 8.6 MB (46.67%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        if(n<=1) return n;
-        int b=(n*(2+(n-1)*2))/2;
-        int a=(n*(4+(n-1)*2))/2;
-        while(b){
-           a = a%b;
-           swap(a, b) ;
-        }
-        return a;
+        // if(n<=1) return n;
+        // int b=(n*(2+(n-1)*2))/2;
+        // int a=(n*(4+(n-1)*2))/2;
+        // while(b){
+        //    a = a%b;
+        //    swap(a, b) ;
+        // }
+        // return a;
+        return n;
     }
 };
