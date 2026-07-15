b7226179393a72c25a8a51aa62f9e5dfd8ac4ee4
Omkar Yandralwar <ody17044@gmail.com>
2026-07-15 09:44:58 +0530

    Time: 277 ms (11.2%), Space: 8.6 MB (9.11%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -4,23 +4,9 @@ public:
         if(n<=1) return n;
         int sumOdd=(n*(2+(n-1)*2))/2;
         int sumEven=(n*(4+(n-1)*2))/2;
-        stack<int> even;
-        stack<int> odd;
-        for(int i=1;i<=sumOdd/2;i++){
-            if(sumOdd%i == 0) odd.push(i);
-            if(sumEven%i == 0) even.push(i);
+        for(int i=sumOdd/2;i>0;i--){
+            if(sumOdd%i==0 && sumEven%i==0) return i;
         }
-        int ans;
-        while(!odd.empty() && !even.empty()){
-            if(odd.top()==even.top()){
-                ans = even.top();
-                break;
-            }else if(odd.top()<even.top()){
-                even.pop();
-            }else if(odd.top()>even.top()){
-                odd.pop();
-            }
-        }
-        return ans;
+        return 0;
     }
 };
