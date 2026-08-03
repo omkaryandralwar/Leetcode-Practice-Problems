bb8e7d5ef4c93b97b3697e1daaeb89ecf1bb8538
Omkar Yandralwar <ody17044@gmail.com>
2026-08-04 00:06:10 +0530

    Time: 20 ms (63.62%), Space: 107.2 MB (53.7%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int> ans(temperatures.size(), 0);
+        stack<int> st;
+        for(int i=0;i<temperatures.size();i++){
+            while(!st.empty() && temperatures[st.top()]<temperatures[i]){
+                int j=st.top();
+                st.pop();
+                ans[j]=i-j;
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+};
